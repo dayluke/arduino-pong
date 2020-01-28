71de73cfc4912311fe08d563008e5c849abcd401
HDonovan96 <harry.bd11@gmail.com>
2020-01-28 16:20:23 +0000

    Edited 'arduino-pong.ino' to read data from ultrasonic sensor

--- arduino-pong/arduino-pong.ino
+++ arduino-pong/arduino-pong.ino
@@ -2,12 +2,18 @@ const int ledPin = 13;
 
 int incomingByte;
 
+// Ultrasonic pingPin.
+const int pingPin = 7;
+// Ultrasonic echoPin.
+const int echoPin = 6;
+
 void setup() {
   Serial.begin(9600);
   pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
+  // Code for passing potentiometer results to Unity.
   if (Serial.available() > 0) {
     incomingByte = Serial.read();
     if (incomingByte == 'H') {
@@ -22,7 +28,22 @@ void loop() {
       Serial.print(analogRead(A4));
       Serial.print("-");
       Serial.println(analogRead(A5));
-    }
-    
+    } 
   }
+
+  // Code for passing ultrasonic sensor to Unity.
+  long duration;
+  long cm;
+  pinMode(pingPin, OUTPUT);
+  
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pingPin, LOW);
+
+  pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
+
+  Serial.println(duration);
 }
